6bbecf8afe23310663a4d0f9426f54a86d7fcb7d
Daniel Dallos <daniel.dallos@theoplayer.com>
2024-02-09 15:23:02 +0100

    use DASH instead of HLS

--- flutter_theoplayer_sdk/example/integration_test/plugin_integration_test.dart
+++ flutter_theoplayer_sdk/example/integration_test/plugin_integration_test.dart
@@ -63,7 +63,7 @@ void main() {
     await tester.pumpAndSettle();
     player.setSource(
       SourceDescription(sources: [
-          TypedSource(src: "https://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8"),
+          TypedSource(src: "https://cdn.theoplayer.com/video/dash/big_buck_bunny/BigBuckBunny_10s_simple_2014_05_09.mpd"),
         ]
       )
     );
