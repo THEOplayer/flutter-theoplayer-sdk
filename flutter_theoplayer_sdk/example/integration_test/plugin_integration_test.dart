9347870acb75a605f05bb9b4c064d0e44d2f1d5e
Daniel Dallos <daniel.dallos@theoplayer.com>
2024-02-13 12:05:01 +0100

    improved tests

--- flutter_theoplayer_sdk/example/integration_test/plugin_integration_test.dart
+++ flutter_theoplayer_sdk/example/integration_test/plugin_integration_test.dart
@@ -8,6 +8,7 @@
 
 
 
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -17,26 +18,6 @@ import 'test_app.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  /*
-  THEOplayer? player;
-
-  Future<void> _setupPlayer (){
-    
-    final ready = Completer();
-
-    player = THEOplayer(
-      theoPlayerConfig: THEOplayerConfig(
-        license: "sZP7IYe6T6PeISfZ3QBr0Ozt3lg6FSa_IuC-TSeo0ZzL0QP1ISUKTD313Kh6FOPlUY3zWokgbgjNIOf9flIKCLh_3ufZFSxlISB-3uXgCZzr0SfZFS0t3l5t3Qfz0l5cCmfVfK4_bQgZCYxNWoryIQXzImf90Sbk0Sft3LCi0u5i0Oi6Io4pIYP1UQgqWgjeCYxgflEc3L5t0l0Z0ufk3SbrFOPeWok1dDrLYtA1Ioh6TgV6v6fVfKcqCoXVdQjLUOfVfGxEIDjiWQXrIYfpCoj-fgzVfKxqWDXNWG3ybojkbK3gflNWfKcqCoXVdQjLUOfVfGxEIDjiWQXrIYfpCoj-fgzVfG3edt06TgV6dwx-Wuh6FOP1WKxZWogef6i6CDrebKjNIwxof6i6IKgZIYxof6i6dDjLf6i6UQg9ID_6FOPzUKjLf6i6Uo46Wt06Ymi6bo4pIXjNWYAZIY3LdDjpflNzbG4gFOPKIDXzUYPgbZf9Dkkj",
-        androidConfiguration: AndroidConfig(useHybridComposition: true)        ),
-      onCreate: (){
-        ready.complete();
-      }
-    );
-
-    return ready.future;
-  }
-  */
 
   testWidgets('Test player created', (WidgetTester tester) async {
     await tester.pumpWidget(const TestApp());
@@ -54,23 +35,31 @@ void main() {
     final chromlessPlayerView = find.byKey(const Key('testChromelessPlayer'));
     final player = (tester.firstElement(chromlessPlayerView).widget as ChromelessPlayer).player;
     await tester.pumpAndSettle();
+
     print("Testing isInitialized()");
     expect(player.isInitialized(), isTrue, reason: "Testing isInitialized()");
+
     print("Testing isPaused()");
     expect(player.isPaused(), isTrue, reason: "Testing isPaused()");
+
     player.setMuted(true);
-    //player.setAutoplay(true);
-    await tester.pumpAndSettle();
+
+    print("Setting source");
+    String sourceUrl = "https://cdn.theoplayer.com/video/dash/big_buck_bunny/BigBuckBunny_10s_simple_2014_05_09.mpd";
+    if (!kIsWeb && Platform.isIOS) {
+      print("Setting iOS source");
+      sourceUrl = "https://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8";
+    }
     player.setSource(
       SourceDescription(sources: [
-          TypedSource(src: "https://cdn.theoplayer.com/video/dash/big_buck_bunny/BigBuckBunny_10s_simple_2014_05_09.mpd"),
+          TypedSource(src: sourceUrl),
         ]
       )
     );
-    await tester.pumpAndSettle();
+
     player.play();
-    await tester.pumpAndSettle();
-    await tester.pump(const Duration(seconds: 30));
+
+    await tester.pump(const Duration(seconds: 10));
     print("Testing playback duration():  ${player.getDuration()}");
     expect(player.getDuration() >= 5, isTrue );
 
--- flutter_theoplayer_sdk/example/integration_test/test_app.dart
+++ flutter_theoplayer_sdk/example/integration_test/test_app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:theoplayer/theoplayer.dart';
 
+// Test license to load sources from localhost and theoplayer.com domains
+const TEST_LICENSE = String.fromEnvironment("TEST_LICENSE", defaultValue: "");
+
 class TestApp extends StatefulWidget {
   const TestApp({super.key});
 
@@ -15,9 +18,15 @@ class _TestAppState extends State<TestApp> {
   void initState() {
     super.initState();
 
+    if (TEST_LICENSE != "") {
+      print("Using test license");
+    } else {
+      print("Using empty license");
+    }
+
     player = THEOplayer(
         theoPlayerConfig: THEOplayerConfig(
-          license: "sZP7IYe6T6PeISfZ3QBr0Ozt3lg6FSa_IuC-TSeo0ZzL0QP1ISUKTD313Kh6FOPlUY3zWokgbgjNIOf9flIKCLh_3ufZFSxlISB-3uXgCZzr0SfZFS0t3l5t3Qfz0l5cCmfVfK4_bQgZCYxNWoryIQXzImf90Sbk0Sft3LCi0u5i0Oi6Io4pIYP1UQgqWgjeCYxgflEc3L5t0l0Z0ufk3SbrFOPeWok1dDrLYtA1Ioh6TgV6v6fVfKcqCoXVdQjLUOfVfGxEIDjiWQXrIYfpCoj-fgzVfKxqWDXNWG3ybojkbK3gflNWfKcqCoXVdQjLUOfVfGxEIDjiWQXrIYfpCoj-fgzVfG3edt06TgV6dwx-Wuh6FOP1WKxZWogef6i6CDrebKjNIwxof6i6IKgZIYxof6i6dDjLf6i6UQg9ID_6FOPzUKjLf6i6Uo46Wt06Ymi6bo4pIXjNWYAZIY3LdDjpflNzbG4gFOPKIDXzUYPgbZf9Dkkj",
+          license: TEST_LICENSE,
           androidConfiguration: AndroidConfig(useHybridComposition: true)        ),
         onCreate: () {
           print("TestApp - THEOplayer - onCreate");
