b6aeddad44c7430e9f0429d04a048dabbf78c89c
Daniel Dallos <daniel.dallos@theoplayer.com>
2024-06-05 13:45:44 +0200

    enable surface creation

--- flutter_theoplayer_sdk/lib/theoplayer_view.dart
+++ flutter_theoplayer_sdk/lib/theoplayer_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:theoplayer_platform_interface/theoplayer_platform_interface.dart';
 import 'package:theoplayer_platform_interface/theopalyer_config.dart';
+import 'package:theoplayer_platform_interface/helpers/logger.dart';
 
 //TODO: move this into theoplayer.dart (?)
 
@@ -17,8 +18,25 @@ class THEOplayerView extends StatefulWidget {
 }
 
 class _THEOplayerViewState extends State<THEOplayerView> {
+
+  bool loaded = false;
+  int textureId = -1;
+
+  @override
+  initState() {
+    super.initState();
+    TheoplayerPlatform.instance.initalize(widget.theoPlayerConfig, (playerId){
+      setState(() {
+        debugLog("[THEOplayerView] Native initialized callback: $playerId");
+        textureId = playerId;
+        loaded = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return TheoplayerPlatform.instance.buildView(context, widget.theoPlayerConfig, widget.onCreated);
+    debugLog("[THEOplayerView] loaded: $loaded");
+    return loaded ? TheoplayerPlatform.instance.buildView(context, widget.theoPlayerConfig, widget.onCreated, textureId) : Container();
   }
 }
