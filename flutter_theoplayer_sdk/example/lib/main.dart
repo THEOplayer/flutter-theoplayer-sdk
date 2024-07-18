a754ec9e70e5eb5e083b7c807909d7a26bb9f780
Daniel Dallos <daniel.dallos@theoplayer.com>
2024-07-18 16:12:31 +0200

    added basic missing license notification

--- flutter_theoplayer_sdk/example/lib/main.dart
+++ flutter_theoplayer_sdk/example/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:theoplayer/theoplayer.dart';
-import 'package:theoplayer/widget/fullscreen_widget.dart';
 import 'package:theoplayer_example/player_widgets/current_time_widget.dart';
 import 'package:theoplayer_example/player_widgets/player_ui_widget.dart';
 import 'package:theoplayer_example/player_widgets/texture_widgets/aspect_ratio_chromeless_widget.dart';
+import 'package:theoplayer_example/player_widgets/texture_widgets/aspect_ratio_custom_fullscreen_widget.dart';
 
 // use your THEOplayer Flutter license here from https://portal.theoplayer.com
 // without a license the player only accepts URLs from 'localhost' or 'theoplayer.com' domains
@@ -54,6 +54,7 @@ class _MyAppState extends State<MyApp> {
         ),
         onCreate: () {
           print("main - THEOplayer - onCreate");
+          player.setAutoplay(true);
           player.setAllowBackgroundPlayback(true);
         });
   }
@@ -132,15 +133,17 @@ class _MyAppState extends State<MyApp> {
                             children: [
                               const Text("Sources"),
                               FilledButton(
-                                onPressed: () => {
+                                onPressed: () {
+                                  _licenseConfigCheckDialog(context);
                                   player.setSource(SourceDescription(sources: [
                                     TypedSource(src: "https://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8"),
-                                  ]))
+                                  ]));
                                 },
                                 child: const Text("Basic source"),
                               ),
                               FilledButton(
-                                onPressed: () => {
+                                onPressed: () {
+                                  _licenseConfigCheckDialog(context);
                                   player.setSource(SourceDescription(sources: [
                                     TypedSource(
                                         src: "https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears_sd.mpd",
@@ -148,12 +151,13 @@ class _MyAppState extends State<MyApp> {
                                           widevine: WidevineDRMConfiguration(
                                               licenseAcquisitionURL: "https://proxy.uat.widevine.com/proxy?provider=widevine_test"),
                                         )),
-                                  ]))
+                                  ]));
                                 },
                                 child: const Text("Widevine source"),
                               ),
                               FilledButton(
-                                onPressed: () => {
+                                onPressed: () {
+                                  _licenseConfigCheckDialog(context);
                                   player.setSource(SourceDescription(sources: [
                                     TypedSource(
                                         src: "https://fps.ezdrm.com/demo/video/ezdrm.m3u8",
@@ -165,12 +169,13 @@ class _MyAppState extends State<MyApp> {
                                             headers: null,
                                           ),
                                         )),
-                                  ]))
+                                  ]));
                                 },
                                 child: const Text("Fairplay EZDRM source - iOS"),
                               ),
                               FilledButton(
-                                onPressed: () => {
+                                onPressed: () {
+                                  _licenseConfigCheckDialog(context);
                                   player.setSource(SourceDescription(sources: [
                                     TypedSource(
                                         src:
@@ -185,7 +190,7 @@ class _MyAppState extends State<MyApp> {
                                             licenseAcquisitionURL: "https://wv-keyos.licensekeyserver.com",
                                           ),
                                         )),
-                                  ]))
+                                  ]));
                                 },
                                 child: const Text("Widevine KeyOS source - Android"),
                               ),
@@ -227,5 +232,41 @@ class _MyAppState extends State<MyApp> {
     print("audio target quality: ${player.getAudioTracks().first.targetQuality?.uid}");
     print("audio active quality: ${player.getAudioTracks().first.activeQuality?.uid}");
     print("allowBackgroundPlayback: ${player.allowBackgroundPlayback()}");
+  }
+
+  Future<void> _licenseConfigCheckDialog(BuildContext context) async {
+    if (PLAYER_LICENSE != "") {
+      //ok
+      return;
+    }
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('License configuration needed!'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Your forgot to configure your license!'),
+                SizedBox(height: 8,),
+                Text('Without a license, THEOplayer can only play sources from `theoplayer.com`'),
+                SizedBox(height: 8,),
+                Text('Get your license from `https://portal.theoplayer.com!`'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
